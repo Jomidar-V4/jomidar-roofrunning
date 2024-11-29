aad7f8ec6c2c5d8fa38a7fdd146852fdabed17f4
Jomidar-V4 <codemwork24@gmail.com>
2024-11-29 21:37:47 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,34 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+name         'jomidar-roofrunning'
+version      '2.0.1'
+description  'A multi-framework roof running'
+author       'Hasib'
+
+shared_scripts {
+    'cfg.lua'
+}
+
+server_scripts {
+    'sv.lua',
+}
+
+client_scripts {
+    'cl.lua',
+}
+dependencies {
+    'qb-core',
+    'NopixelV4NPCDialogue',
+    'skillchecks',
+    'j-textui'
+}
+ui_page "web/ui.html"
+
+files {
+	"web/ui.html",
+    "web/css.css",
+    "web/js.js",
+    'web/fonts/*.ttf',
+}
